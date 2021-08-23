0452b95969c8e0721c7cef2b4c93ff5987cc99fa
zeqas <casperi52400@gmail.com>
2021-08-23 19:01:46 +0800

    feat: install express-session connect-flash, confirm user password and unique email

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
@@ -10,8 +13,22 @@ app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({ 
+  secret: 'secret', 
+  resave: false, 
+  saveUninitialized: false 
+}))
+app.use(flash())
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
+})
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
