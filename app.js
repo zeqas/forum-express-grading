8c790d99dcf1a004d8265ec45f947fcd3a0b4996
zeqas <casperi52400@gmail.com>
2021-08-23 16:28:16 +0800

    feat: add admin and user index page

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const handlebars = require('express-handlebars') // 引入 handlebars
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+// 設定 view engine 使用 handlebars
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js')
+
+module.exports = app => {
+
+  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+
+  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
+  app.get('/restaurants', restController.getRestaurants)
+
+  // 連到 /admin 頁面就轉到 /admin/restaurants
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  app.get('/admin/restaurants', adminController.getRestaurants)
+}
