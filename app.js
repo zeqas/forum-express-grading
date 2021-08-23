23c86de557f14801468e1f5cf7b86c055eb43639
zeqas <casperi52400@gmail.com>
2021-08-23 15:46:49 +0800

    chore: initialize project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const handlebars = require('express-handlebars') // 引入 handlebars
+const app = express()
+const port = 3000
+
+app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+
+require('./routes')(app)
+
+module.exports = app
