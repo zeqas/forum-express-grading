2d189da4e12bc340a01512f2d86985421db9f771
zeqas <casperi52400@gmail.com>
2021-08-24 00:17:08 +0800

    feat: add isAdmin to users table and authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,28 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  //如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
-  app.get('/restaurants', restController.getRestaurants)
-
-  // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-
-  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
-  
+
   app.post('/signup', userController.signUp)
 
   app.get('/signin', userController.signInPage)
