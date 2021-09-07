b5592607c67f1485e33d1e8042226da6edcef55b
zeqas <casperi52400@gmail.com>
2021-09-07 11:12:26 +0800

    refactor: GET /api/admin/restaurants/:id

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -66,12 +66,8 @@ const adminController = {
   },
   // Read the restaurant
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: [Category]
-    }).then(restaurant => {
-      return res.render('admin/restaurant', {
-        restaurant: restaurant.toJSON()
-      })
+    adminService.getRestaurant(req, res, (data) => {
+      return res.render('admin/restaurant', data)
     })
   },
   // Update
--- services/adminService.js
+++ services/adminService.js
@@ -5,7 +5,12 @@ const Category = db.Category
 const adminService = {
   getRestaurants: (req, res, callback) => {
     return Restaurant.findAll({include: [Category]}).then(restaurants => {
-      callback({ restaurants : restaurants})
+      callback({ restaurants : restaurants })
+    })
+  },
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {include: [Category]}).then(restaurant => {
+      callback({ restaurant: restaurant })
     })
   }
 }
