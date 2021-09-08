dbea7c16833d69045d3155d7cc59155c74310488
zeqas <casperi52400@gmail.com>
2021-09-08 10:56:01 +0800

    feat: options in getRestaurant(s)

--- services/adminService.js
+++ services/adminService.js
@@ -7,13 +7,22 @@ const Category = db.Category
 
 const adminService = {
   getRestaurants: (req, res, callback) => {
-    return Restaurant.findAll({include: [Category]}).then(restaurants => {
-      callback({ restaurants : restaurants })
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => {
+        callback({ restaurants })
     })
   },
   getRestaurant: (req, res, callback) => {
-    return Restaurant.findByPk(req.params.id, {include: [Category]}).then(restaurant => {
-      callback({ restaurant: restaurant })
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurant => {
+      callback({ restaurant })
     })
   },
   postRestaurant: (req, res) => {
